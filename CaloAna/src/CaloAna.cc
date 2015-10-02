307146e8fe0832c4bd1a70b2a5e89baacf1fcc62
Chris Pinkenburg <pinkenburg@bnl.gov>
2015-10-02 13:42:38 -0400

    example for loops needed to access calorimeter data

---
+++ CaloAna/src/CaloAna.cc
@@ -0,0 +1,246 @@
+#include "CaloAna.h"
+
+// G4Hits includes
+#include <g4main/PHG4HitContainer.h>
+#include <g4main/PHG4Hit.h>
+
+// G4Cells includes
+#include <g4detectors/PHG4CylinderCellGeomContainer.h>
+#include <g4detectors/PHG4CylinderCellGeom.h>
+#include <g4detectors/PHG4CylinderCellContainer.h>
+#include <g4detectors/PHG4CylinderCell.h>
+
+// Tower includes
+#include <g4cemc/RawTowerGeom.h>
+#include <g4cemc/RawTowerContainer.h>
+#include <g4cemc/RawTower.h>
+
+// Cluster includes
+#include <g4cemc/RawClusterContainer.h>
+#include <g4cemc/RawCluster.h>
+
+
+#include <fun4all/Fun4AllHistoManager.h>
+#include <fun4all/Fun4AllReturnCodes.h>
+
+#include <fun4all/getClass.h>
+
+#include <TFile.h>
+#include <TNtuple.h>
+
+#include <cassert>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+CaloAna::CaloAna(const std::string &name, const std::string &filename):
+  SubsysReco( name ),
+  outfilename(filename),
+  hm(NULL),
+  outfile(NULL),
+  g4hitntuple(NULL),
+  g4cellntuple(NULL),
+  towerntuple(NULL),
+  clusterntuple(NULL)
+{}
+
+CaloAna::~CaloAna()
+{
+  delete hm;
+  delete g4hitntuple;
+  delete g4cellntuple;
+  delete towerntuple;
+  delete clusterntuple;
+}
+
+
+int
+CaloAna::Init( PHCompositeNode* )
+{
+  hm = new  Fun4AllHistoManager(Name());
+  // create and register your histos (all types) here
+  // TH1 *h1 = new TH1F("h1",....)
+  // hm->registerHisto(h1);
+  outfile = new TFile(outfilename.c_str(), "RECREATE");
+  g4hitntuple = new TNtuple("hitntup", "G4Hits", "x0:y0:z0:x1:y1:z1:edep");
+  g4cellntuple = new TNtuple("cellntup", "G4Cells", "phi:eta:edep");
+  towerntuple = new TNtuple("towerntup", "Towers", "phi:eta:energy");
+  clusterntuple  = new TNtuple("clusterntup", "Clusters", "phi:eta:energy:towers");
+  return 0;
+}
+
+int
+CaloAna::process_event( PHCompositeNode* topNode )
+{
+  // For the calorimeters we have the following node name convention
+  // where detector is the calorimeter name (CEMC, HCALIN, HCALOUT)
+  // this is the order in which they are reconstructed
+  //  G4HIT_<detector>: G4 Hits
+  //  G4CELL_<detector>: Cells (combined hits inside a cell - scintillator, eta/phi bin)
+  //  TOWER_SIM_<detector>: simulated tower (before pedestal and noise)
+  //  TOWER_RAW_<detector>: Raw Tower (adc/tdc values - from sims or real data)
+  //  TOWER_CALIB_<detector>: Calibrated towers
+  //  CLUSTER_<detector>: clusters
+  process_g4hits(topNode);
+  //  process_g4cells(topNode);
+  process_towers(topNode);
+  process_clusters(topNode);
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int
+CaloAna::process_g4hits( PHCompositeNode* topNode )
+{
+
+  ostringstream nodename;
+  string detector = "HCALIN";
+
+  // loop over the G4Hits
+  nodename.str("");
+  nodename << "G4HIT_" << detector;
+  PHG4HitContainer *hits = findNode::getClass<PHG4HitContainer>(topNode, nodename.str().c_str());
+  if (hits)
+    {
+      // this returns an iterator to the beginning and the end of our G4Hits
+      PHG4HitContainer::ConstRange hit_range = hits->getHits();
+      for ( PHG4HitContainer::ConstIterator hit_iter = hit_range.first ; hit_iter !=  hit_range.second; hit_iter++ )
+
+	{
+	  // the pointer to the G4Hit is hit_iter->second
+	  g4hitntuple->Fill(hit_iter->second->get_x(0),
+			    hit_iter->second->get_y(0),
+			    hit_iter->second->get_z(0),
+			    hit_iter->second->get_x(1),
+			    hit_iter->second->get_y(1),
+			    hit_iter->second->get_z(1),
+			    hit_iter->second->get_edep());
+	}
+    }
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int
+CaloAna::process_g4cells( PHCompositeNode* topNode )
+{
+  ostringstream nodename;
+  ostringstream geonodename;
+  string detector = "HCALIN";
+
+  // loop over the G4Hits
+  nodename.str("");
+  nodename << "G4CELL_" << detector;
+  geonodename.str("");
+  geonodename << "CYLINDERCELLGEOM_" << detector;
+  PHG4CylinderCellGeomContainer* cellgeos = findNode::getClass<PHG4CylinderCellGeomContainer>(topNode, geonodename.str().c_str());
+  if (!cellgeos)
+    {
+      return Fun4AllReturnCodes::EVENT_OK;
+    }
+  PHG4CylinderCellContainer *cells = findNode::getClass<PHG4CylinderCellContainer>(topNode, nodename.str().c_str());
+  if (cells)
+    {
+      int previouslayer = -1;
+      PHG4CylinderCellGeom *cellgeom = NULL;
+      // this returns iterators to the beginning and the end of the cell map
+      PHG4CylinderCellContainer::ConstRange cell_range = cells->getCylinderCells();
+      for ( PHG4CylinderCellContainer::ConstIterator cell_iter = cell_range.first ; cell_iter !=  cell_range.second; cell_iter++ )
+	{
+	  int phibin = cell_iter->second->get_binphi();
+	  int etabin = cell_iter->second->get_bineta();
+	  int layer = cell_iter->second->get_layer();
+	  // to search the map fewer times, cache the geom object until the layer changes
+	  if (layer != previouslayer)
+	    {
+	      cellgeom = cellgeos->GetLayerCellGeom(layer);
+	      previouslayer = layer;
+	    }
+	  assert(cellgeom != NULL);
+	  double phi = cellgeom->get_phicenter(phibin);
+	  double eta = cellgeom->get_etacenter(etabin);
+	  g4cellntuple->Fill(
+			     phi,
+			     eta,
+			     cell_iter->second->get_edep());
+
+	}
+    }
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int
+CaloAna::process_towers( PHCompositeNode* topNode )
+{
+  ostringstream nodename;
+  ostringstream geonodename;
+  string detector = "HCALIN";
+
+  // loop over the G4Hits
+  nodename.str("");
+  nodename << "TOWER_CALIB" << detector;
+  geonodename.str("");
+  geonodename << "TOWERGEOM_" << detector;
+  RawTowerGeom* towergeom = findNode::getClass<RawTowerGeom>(topNode, geonodename.str().c_str());
+  if (!towergeom)
+    {
+      return Fun4AllReturnCodes::EVENT_OK;
+    }
+  RawTowerContainer *towers = findNode::getClass<RawTowerContainer>(topNode, nodename.str().c_str());
+  if (towers)
+    {
+      // again pair of iterators to begin and end of tower map
+      RawTowerContainer::ConstRange tower_range = towers->getTowers();
+      for ( RawTowerContainer::ConstIterator tower_iter = tower_range.first ; tower_iter !=  tower_range.second; tower_iter++ )
+
+	{
+	  int phibin = tower_iter->second->get_binphi();
+	  int etabin = tower_iter->second->get_bineta();
+	  double phi = towergeom->get_phicenter(phibin);
+	  double eta = towergeom->get_etacenter(etabin);
+	  towerntuple->Fill(phi,
+			    eta,
+			    tower_iter->second->get_energy());
+	}
+    }
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int
+CaloAna::process_clusters( PHCompositeNode* topNode )
+{
+  ostringstream nodename;
+  string detector = "HCALIN";
+
+  // loop over the G4Hits
+  nodename.str("");
+  nodename << "CLUSTER_" << detector;
+  RawClusterContainer *clusters = findNode::getClass<RawClusterContainer>(topNode, nodename.str().c_str());
+  if (clusters)
+    {
+      RawClusterContainer::ConstRange cluster_range = clusters->getClusters();
+      for ( RawClusterContainer::ConstIterator cluster_iter = cluster_range.first ; cluster_iter !=  cluster_range.second; cluster_iter++ )
+	{
+	  clusterntuple->Fill(cluster_iter->second->get_phi(),
+			      cluster_iter->second->get_eta(),
+			      cluster_iter->second->get_energy(),
+			      cluster_iter->second->getNTowers());
+	}
+    }
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int
+CaloAna::End(PHCompositeNode * topNode)
+{
+  outfile->cd();
+  g4hitntuple->Write();
+  g4cellntuple->Write();
+  towerntuple->Write();
+  clusterntuple->Write();
+  outfile->Write();
+  outfile->Close();
+  delete outfile;
+  hm->dumpHistos(outfilename, "UPDATE");
+  return 0;
+}
+
---
+++ CaloAna/src/CaloAna.h
@@ -0,0 +1,46 @@
+#ifndef CALOANA_H__
+#define CALOANA_H__
+
+#include <fun4all/SubsysReco.h>
+
+// Forward declarations
+class Fun4AllHistoManager;
+class PHCompositeNode;
+class TFile;
+class TNtuple;
+
+class CaloAna: public SubsysReco
+{
+ public:
+
+  //! constructor
+  CaloAna( const std::string &name = "CaloAna", const std::string &fname = "MyNtuple.root");
+
+  //! destructor
+  virtual ~CaloAna();
+
+  //! full initialization
+  int Init(PHCompositeNode *);
+
+  //! event processing method
+  int process_event(PHCompositeNode *);
+
+  //! end of run method
+  int End(PHCompositeNode *);
+
+  int process_g4hits(PHCompositeNode *);
+  int process_g4cells(PHCompositeNode *);
+  int process_towers(PHCompositeNode *);
+  int process_clusters(PHCompositeNode *);
+
+protected:
+  std::string outfilename;
+  Fun4AllHistoManager *hm;
+  TFile *outfile; 
+  TNtuple *g4hitntuple;
+  TNtuple *g4cellntuple;
+  TNtuple *towerntuple;
+  TNtuple *clusterntuple;
+};
+
+#endif 

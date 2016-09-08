07c906a9404c20822d77d02129936bcfb1822bd2
Chris Pinkenburg <pinkenburg@bnl.gov>
2016-09-08 13:57:14 -0400

    set detector name via macro

--- CaloAna/macro/CaloAna.C
+++ CaloAna/macro/CaloAna.C
@@ -5,6 +5,9 @@ void CaloAna(const char *fname = "/phenix/scratch/pinkenbu/G4sPHENIXCells.root")
   gSystem->Load("libcaloana");
   Fun4AllServer *se = Fun4AllServer::instance();
   CaloAna *ca = new CaloAna("CALOANA","out.root");
+  // choose CEMC, HCALIN or HCALOUT or whatever you named your
+  // calorimeter
+  ca->Detector("HCALIN");
   se->registerSubsystem(ca);
   Fun4AllInputManager *in = new Fun4AllDstInputManager("in");
   in->fileopen(fname);

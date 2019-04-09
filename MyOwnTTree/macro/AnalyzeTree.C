57081d66308b3408226db5710f1779871d5b68a5
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-04-09 16:49:12 -0400

    run under root6

--- MyOwnTTree/macro/AnalyzeTree.C
+++ MyOwnTTree/macro/AnalyzeTree.C
@@ -1,3 +1,15 @@
+#pragma once
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
+#include <fun4all/SubsysReco.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllInputManager.h>
+#include <fun4all/Fun4AllDstInputManager.h>
+#include <mytree/AnalyzeSimpleTree.h>
+
+R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libmytree.so)
+#endif
+
 void  AnalyzeTree()
 {
   gSystem->Load("libmytree.so");

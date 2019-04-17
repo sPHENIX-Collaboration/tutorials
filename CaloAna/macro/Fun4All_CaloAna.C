078ff5da4ec445cd778034f885f5e23797f020a6
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-04-17 17:35:39 -0400

    work with root6 and new calorimeter cells

--- CaloAna/macro/Fun4All_CaloAna.C
+++ CaloAna/macro/Fun4All_CaloAna.C
@@ -1,13 +1,25 @@
-void CaloAna(const char *fname = "/phenix/scratch/pinkenbu/G4sPHENIXCells.root")
+#pragma once
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
+#include <fun4all/SubsysReco.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllInputManager.h>
+#include <fun4all/Fun4AllDstInputManager.h>
+
+#include <caloana/CaloAna.h>
+
+R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libcaloana.so)
+#endif
+
+void Fun4All_CaloAna(const char *fname = "/sphenix/sim/sim01/tutorials/CaloAna/G4sPHENIX_Pythia8.root")
 {
-  gSystem->Load("libg4vertex_io");
-  gSystem->Load("libg4hough_io");
+  gSystem->Load("libg4dst");
   gSystem->Load("libcaloana");
   Fun4AllServer *se = Fun4AllServer::instance();
   CaloAna *ca = new CaloAna("CALOANA","out.root");
   // choose CEMC, HCALIN or HCALOUT or whatever you named your
   // calorimeter
-  ca->Detector("HCALIN");
+  ca->Detector("CEMC");
   se->registerSubsystem(ca);
   Fun4AllInputManager *in = new Fun4AllDstInputManager("in");
   in->fileopen(fname);

e8d0508038f8922bd66d59070432262316dde78e
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-04-17 18:13:07 -0400

    works with root6

--- clusters/G4TTree.C
+++ clusters/G4TTree.C
@@ -1,6 +1,25 @@
-void G4TTree(const char *fname, const int nevnt = 0, const char *outfile="towers.root")
+#pragma once
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
+#include <fun4all/SubsysReco.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllInputManager.h>
+#include <fun4all/Fun4AllDstInputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllDstOutputManager.h>
+
+#include <g4histos/G4HitTTree.h>
+#include <g4histos/G4RawTowerTTree.h>
+R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libg4histos.so)
+#endif
+
+void G4TTree(
+  const char *fname = "/sphenix/sim/sim01/tutorials/clusters/G4sPHENIX_Pythia8.root",
+  const int nevnt = 0,
+  const char *outfile="towers.root")
 {
   gSystem->Load("libg4histos");
+  gSystem->Load("libg4dst.so");
   Fun4AllServer *se = Fun4AllServer::instance();
   char detector[100];
   char outnode[100];

6d5e8a5ea9ee393d91346f7c71f4a6a82690ded7
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-04-27 15:27:49 -0400

    add plotting for tdr as function of eta

--- materialscan/plot_matscan.C
+++ materialscan/plot_matscan.C
@@ -123,3 +123,126 @@ void plot_matscan(const char *datfile, const int plot_theta = 0)
   fclose(f);
   return;
 }
+
+void plot_matscan_tdr(const char *datfile, const int plot_theta = 0)
+{
+  FILE *f = fopen(datfile,"r");
+  if (!f)
+    {
+      cout << "file " << datfile << " cannot be opened" << endl;
+      return;
+    }
+  float theta;
+  float phi;
+  float path;
+  float x0;
+  float lamda0;
+  vector<float> thetavec;
+  vector<float> phivec;
+  vector<float> x0vec;
+  vector<float> lamda0vec;
+  float thetamin = -1.10;
+  float thetamax = 1.1;
+  float phimin = 10000;
+  float phimax = -10000;
+  float x0min = 100000;
+  float x0max = -100000;
+  float lamda0min = 100000;
+  float lamda0max = -100000;
+  while(fscanf(f,"%f %f %f %f %f",&theta,&phi,&path,&x0,&lamda0) != EOF)
+    {
+      theta = (theta+90)/180.*M_PI;
+      theta =  log(TMath::Tan(theta/2.));
+      cout << "theta: " << theta
+	   << ", phi: " << phi
+	   << ", path: " << path
+	   << ", x0: " << x0
+	   << ", lamda0: " << lamda0
+	   << endl;
+      if (phi > phimax)
+	{
+	  phimax = phi;
+	}
+      if (phi < phimin)
+	{
+	  phimin = phi;
+	}
+     if (x0 > x0max)
+	{
+	  x0max = x0;
+	}
+      if (x0 < x0min)
+	{
+	  x0min = x0;
+	}
+     if (lamda0 > lamda0max)
+	{
+	  lamda0max = lamda0;
+	}
+      if (lamda0 < lamda0min)
+	{
+	  lamda0min = lamda0;
+	}
+      thetavec.push_back(theta);
+      phivec.push_back(phi);
+      x0vec.push_back(x0);
+      lamda0vec.push_back(lamda0);
+    }
+  float *thetaarr = new float[thetavec.size()];
+  float *phiarr = new float[phivec.size()];
+  float *x0arr = new float[x0vec.size()];
+  float *lamda0arr = new float[lamda0vec.size()];
+  for (int i=0; i<phivec.size(); i++)
+    {
+      thetaarr[i] = thetavec[i];
+      phiarr[i] = phivec[i];
+      x0arr[i] = x0vec[i];
+      lamda0arr[i] = lamda0vec[i];
+    }
+  TCanvas *c = new TCanvas("c1","material scan");
+  char htitlex[100];
+  char htitlelamda[100];
+  char titlexaxis[100];
+  double xmin;
+  double xmax;
+  TGraph *gr = nullptr;
+  TGraph *grl = nullptr;
+
+  if (plot_theta)
+  {
+  sprintf(htitlex,"x0 vs theta");
+  sprintf(htitlelamda,"");
+  sprintf(titlexaxis,"#eta");
+  xmin = thetamin;
+  xmax = thetamax;
+  gr = new TGraph(thetavec.size(),thetaarr,x0arr);
+  grl = new TGraph(thetavec.size(),thetaarr,lamda0arr);
+  }
+  else
+  {
+  sprintf(htitlex,"x0 vs phi");
+  sprintf(htitlelamda,"");
+  sprintf(titlexaxis,"#phi");
+  xmin = phimin - 1;
+  xmax = phimax + 1;
+  gr = new TGraph(phivec.size(),phiarr,x0arr);
+  grl = new TGraph(phivec.size(),phiarr,lamda0arr);
+  }
+  TH2 *hl2 = new TH2F("hlamda2",htitlelamda,2,xmin,xmax,2,0,lamda0max+lamda0max/10.);
+  hl2->SetStats(kFALSE);
+  hl2->GetXaxis()->SetTitle(titlexaxis);
+  hl2->GetXaxis()->SetTitleSize(0.05);
+  hl2->GetXaxis()->SetLabelSize(0.04);
+  hl2->GetXaxis()->SetTitleOffset(0.8);
+  hl2->GetYaxis()->SetTitle("#lambda_{0}");
+  hl2->GetYaxis()->SetTitleSize(0.05);
+  hl2->GetYaxis()->SetTitleOffset(0.6);
+  hl2->GetYaxis()->SetLabelSize(0.04);
+  hl2->Draw();
+  grl->SetMarkerStyle(21);
+  grl->SetMarkerSize(0.1);
+  grl->SetLineWidth(2);
+  grl->Draw("LP");
+  fclose(f);
+  return;
+}

b448da569e77e0b2d5c66bb8b87d71ac1ffcc817
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-07-13 18:23:26 -0400

    use field rescale as argument

--- MagneticField/macros/Print3dFieldMap.C
+++ MagneticField/macros/Print3dFieldMap.C
@@ -3,10 +3,10 @@
 
 #include <fieldmapreadback/FieldMapReadBack.h>
 
-FieldMapReadBack *Print3dFieldMap()
+FieldMapReadBack *Print3dFieldMap(const float magfield_rescale = 1.)
 {
   FieldMapReadBack *fmap = new FieldMapReadBack();
-  fmap->Load3dCartMap("$CALIBRATIONROOT/Field/Map/sphenix3dbigmapxyz.root");
+  fmap->Load3dCartMap("$CALIBRATIONROOT/Field/Map/sphenix3dbigmapxyz.root",magfield_rescale);
   fmap->Verbosity(2);
   fmap->PrintField(0,0,0);
   return fmap;

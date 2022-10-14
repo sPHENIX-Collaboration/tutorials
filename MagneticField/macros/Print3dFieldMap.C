0cdac45b98ce07e9edde135bd2bc7dfc552d9b4e
Chris Pinkenburg <pinkenburg@bnl.gov>
2022-10-14 10:37:04 -0400

    update macros to return fieldmap reader

--- MagneticField/macros/Print3dFieldMap.C
+++ MagneticField/macros/Print3dFieldMap.C
@@ -3,10 +3,15 @@
 
 #include <fieldmapreadback/FieldMapReadBack.h>
 
-FieldMapReadBack *Print3dFieldMap(const float magfield_rescale = 1.)
+R__LOAD_LIBRARY(libfieldmapreadbackread.so)
+
+// our maps can be found under
+//$CALIBRATIONROOT/Field/Map
+
+FieldMapReadBack *Print3dFieldMap(const std::string &mapname, const float magfield_rescale = 1.)
 {
   FieldMapReadBack *fmap = new FieldMapReadBack();
-  fmap->Load3dCartMap("$CALIBRATIONROOT/Field/Map/sphenix3dbigmapxyz.root", magfield_rescale);
+  fmap->Load3dCartMap(mapname, magfield_rescale);
   fmap->Verbosity(2);
   fmap->PrintField(0, 0, 0);
   return fmap;
--- MagneticField/macros/ReadFieldMap.C
+++ MagneticField/macros/ReadFieldMap.C
@@ -8,26 +8,27 @@
 
 #include <fieldmapreadback/FieldMapReadBack.h>
 
-void ReadFieldMap(const int nEvents = 1, const std::string &filename="DST_TRKR_CLUSTER_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000001-04001.root")
+R__LOAD_LIBRARY(libfieldmapreadbackread.so)
+
+// reads fieldmap from the DST. Needs only one event to force the opening
+// and reading of the runwise information, running more just wastes your time
+
+FieldMapReadBack *ReadFieldMap(const int nEvents = 1, const std::string &filename="DST_TRKR_CLUSTER_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000001-04001.root")
 {
   gSystem->Load("libg4dst.so");
   Fun4AllServer *se = Fun4AllServer::instance();
   se->Verbosity(); // set it to 1 if you want event printouts
   FieldMapReadBack *fmap = new FieldMapReadBack();
   se->registerSubsystem(fmap);
-   Fun4AllInputManager *in = new Fun4AllDstInputManager("DSTin");
-   in->fileopen(filename);
-   se->registerInputManager(in);
-//   Fun4AllDummyInputManager *in2 = new Fun4AllDummyInputManager("Zen");
-//   se->registerInputManager(in2);
-   if (nEvents < 0)
-   {
-     return;
-   }
-   se->run(nEvents);
-   se->End();
-   delete se;
-   gSystem->Exit(0);
+  Fun4AllInputManager *in = new Fun4AllDstInputManager("DSTin");
+  in->fileopen(filename);
+  se->registerInputManager(in);
+  if (nEvents < 0)
+  {
+    return;
+  }
+  se->run(nEvents);
+  return fmap;
 }
 
 

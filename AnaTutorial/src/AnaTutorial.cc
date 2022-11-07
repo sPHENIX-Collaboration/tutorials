0d31407432bc109cbb45cf85688abb933a11bbf0
Jin Huang <jhuang@bnl.gov>
2022-11-07 10:01:52 -0500

    gcc12 warning suppression

--- AnaTutorial/src/AnaTutorial.cc
+++ AnaTutorial/src/AnaTutorial.cc
@@ -27,8 +27,12 @@
 #include <g4eval/SvtxEvalStack.h>
 
 /// HEPMC truth includes
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #include <HepMC/GenEvent.h>
 #include <HepMC/GenVertex.h>
+#pragma GCC diagnostic pop
+
 #include <phhepmc/PHHepMCGenEvent.h>
 #include <phhepmc/PHHepMCGenEventMap.h>
 

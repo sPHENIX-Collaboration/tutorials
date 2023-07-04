54863ff59f0a864d7869414a20d1c6bc9ddfba77
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-07-04 16:09:16 -0400

    use bash, source /opt/sphenix/core scripts

--- AnaTutorial/macro/condor_scripts/run_tutorial.sh
+++ AnaTutorial/macro/condor_scripts/run_tutorial.sh
@@ -1,28 +1,28 @@
-#!/bin/csh
+#!/bin/bash
 
-# This is an example script to run the basic macro associated to AnaTutorial. 
+# This is an example bash script to run the basic macro associated to AnaTutorial.
 # It should only be considered an example! You need to modify the base directory and install directory to wherever you are working
 # You should also adjust the line immediately below to mat
-setenv HOME /phenix/u/$LOGNAME # (or /star/u/$LOGNAME or /eic/u/$LOGNAME)
-source /etc/csh.login
-foreach i (/etc/profile.d/*.csh)
-  source $i
-end
-source $HOME/.login
+ export USER="$(id -u -n)"
+ export LOGNAME=${USER}
+ export HOME=/phenix/u/${LOGNAME} # (or /star/u/${LOGNAME} or /sphenix/u/${LOGNAME} or /eic/u/${LOGNAME})
 
 # Set the baseDir to your directory where your macro exists
-set baseDir=/sphenix/user/jdosbo/git/sphenix/tutorials/AnaTutorial/macro
+baseDir=/sphenix/user/jdosbo/git/sphenix/tutorials/AnaTutorial/macro
 # Set the install directory to the path where your AnaTutorial library is installed
-set MYINSTALL=/sphenix/user/jdosbo/install
+MYINSTALL=/sphenix/user/jdosbo/install
 
 echo $baseDir
 
 echo "Processing job "$1
 
 # Source the newest sPHENIX build environment
-source /cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/bin/sphenix_setup.csh -n
+source /opt/sphenix/core/bin/sphenix_setup.sh -n
 # Source the local environment with your AnaTutorial installation
-source /cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/bin/setup_local.csh $MYINSTALL
+source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
+
+# print the environment - needed for debugging (when you ask for help)
+printenv
 
 cd $baseDir
 

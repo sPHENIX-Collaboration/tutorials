a883aca90b540457e83421f1e01cb036a259ffa0
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-05-22 04:56:59 -0400

    cleanup of macros

--- CaloDataAnaRun24pp/macro/Fun4All_CaloTreeGen.C
+++ CaloDataAnaRun24pp/macro/Fun4All_CaloTreeGen.C
@@ -1,5 +1,6 @@
-#pragma once
-#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
+#ifndef MACRO_FUN4ALL_CALOTREEGEN_C
+#define MACRO_FUN4ALL_CALOTREEGEN_C
+
 #include <fun4all/SubsysReco.h>
 #include <fun4all/Fun4AllServer.h>
 #include <fun4all/Fun4AllInputManager.h>
@@ -17,9 +18,8 @@
 R__LOAD_LIBRARY(libfun4all.so)
 R__LOAD_LIBRARY(libffarawobjects.so)
 R__LOAD_LIBRARY(libcaloTreeGen.so)
-#endif
 
-void Fun4All_CaloTreeGen(const int nEvents = 0, const char *listFile = "fileList_withGeo_timingCut_Template.list", const char *inName = "commissioning.root")
+  void Fun4All_CaloTreeGen(const int nEvents = 0, const std::string &listFile = "run43273.list", const std::string &inName = "commissioning.root")
 {
   Fun4AllServer *se = Fun4AllServer::instance();
   recoConsts *rc = recoConsts::instance();
@@ -28,21 +28,21 @@ void Fun4All_CaloTreeGen(const int nEvents = 0, const char *listFile = "fileList
 
   caloTreeGen *calo = new caloTreeGen(inName);
   //What subsystems do you want?
-  calo -> doEMCal(1,"TOWERINFO_CALIB_CEMC");
+  calo->doEMCal(1,"TOWERINFO_CALIB_CEMC");
   //Store EMCal clusters?
-  calo -> doClusters(1,"CLUSTERINFO_CEMC");
+  calo->doClusters(1,"CLUSTERINFO_CEMC");
 
   //Store tower information for each EMCal cluster?
-  calo -> doClusterDetails(1);
+  calo->doClusterDetails(1);
   
   //Store HCal information?
-  calo -> doHCals(1,"TOWERINFO_CALIB_HCALOUT","TOWERINFO_CALIB_HCALIN");
+  calo->doHCals(1,"TOWERINFO_CALIB_HCALOUT","TOWERINFO_CALIB_HCALIN");
 
   //Store ZDC information?
-  calo -> doZDC(1,"TOWERINFO_CALIB_ZDC");
+  calo->doZDC(1,"TOWERINFO_CALIB_ZDC");
 
   //Store GL1 Information?
-  calo -> doTrig(1,"GL1Packet");
+  calo->doTrig(1,"GL1Packet");
   
   
   se->registerSubsystem(calo);
@@ -59,3 +59,5 @@ void Fun4All_CaloTreeGen(const int nEvents = 0, const char *listFile = "fileList
 
   gSystem->Exit(0);
 }
+
+#endif
